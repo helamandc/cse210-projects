18a1ad0455e0aaf1185bb808392560335e9719cd
helamandc <manlehcastillo@gmail.com>
2025-01-13 22:30:31 +1100

    Week 1 - Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        int userNumber = 0;
+        float sum = 0;
+        float average = 0;
+        int count = 0;
+        int largestNum = 0;
+
+        List<int> numberList = new List<int> ();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+            userNumber = int.Parse(number);
+            numberList.Add(userNumber);
+        }
+        while(userNumber != 0);
+
+        foreach (int number in numberList)
+        {
+            sum += number;
+        }
+        
+        count = numberList.Count - 1;
+
+        average = sum / count;
+
+        largestNum = numberList.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+
     }
 }
