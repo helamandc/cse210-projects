af9476bbcfb13e82562671f8a7a7ea6b999d61e7
helamandc <manlehcastillo@gmail.com>
2025-01-13 19:54:04 +1100

    Week 1 - Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradep = int.Parse(grade);
+        string letter = "";
+
+        if (gradep >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradep >= 80 && gradep < 90)
+        {
+            letter = "B";
+        }
+        else if (gradep >= 70 && gradep < 80)
+        {
+            letter = "C";
+        }
+        else if (gradep >= 60 && gradep < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.Write($"Your letter grade is {letter}.");
+
+        if (gradep >= 70)
+        {
+            Console.Write("Congratulations on passing the course!");
+        }
+        else
+        {
+            Console.Write("\nStay motivated so you can pass next time.");
+        }
+
     }
 }
