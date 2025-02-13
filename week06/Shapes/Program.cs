ce74e34d55af7691dd7e185c3fc9943b5df68858
helamandc <manlehcastillo@gmail.com>
2025-02-14 10:28:35 +1100

    Shape activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> differentShapes = new List<Shape>();
+
+        Circle circle = new Circle("Orange", 2.50);
+        differentShapes.Add(circle);
+
+        Rectangle rectangle = new Rectangle("Maroon", 5.0, 6.7);
+        differentShapes.Add(rectangle);
+
+        Square square = new Square("Violet", 9.50);
+        differentShapes.Add(square);
+
+        foreach (Shape s in differentShapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"This the area {area:F2} of {color} shape.");
+        }
+
     }
 }
